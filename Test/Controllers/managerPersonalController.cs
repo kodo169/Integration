687b0af26cd8eacfdb6fd59004aeeb6323937c4d
Vũ Huỳnh <vuhuynh1692003@gmail.com>
2024-05-04 18:03:07 +0700

    complete day 4-5-24

---
+++ Test/Controllers/managerPersonalController.cs
@@ -0,0 +1,28 @@
+﻿using Integration.Data;
+using Integration.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Integration.Controllers
+{
+    public class managerPersonalController : Controller
+    {
+        private readonly HrmContext _dataSQLServer;
+        private readonly MydbContext _dataMySQLServer;
+        public managerPersonalController(HrmContext dataSQLServer, MydbContext dataMySQLServer)
+        {
+            _dataSQLServer = dataSQLServer;
+            _dataMySQLServer = dataMySQLServer;
+        }
+
+        public IActionResult Index()
+        {
+            var inforStaffHRM = _dataSQLServer.Personals
+                .Include(p => p.Employments)
+                .Where(p => p.Employments.Any(e => e.PersonalId == p.PersonalId))
+                .ToList();
+            return View(inforStaffHRM);
+        }
+
+    }
+}
